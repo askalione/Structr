07cfc30fd58ac83e26e7e80c46444055d86d7f8e
Alexey Drapash <godhot@yandex.ru>
2020-01-23 11:00:26 +0300

    Upd Abstractions, IO, Domain

--- src/Structr.Domain/IModifiable.cs
+++ src/Structr.Domain/IModifiable.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Structr.Domain
 {
-    public interface IModifiable
+    public interface IModifiable : IAuditable
     {
         DateTime DateModified { get; }
     }
--- src/Structr.Domain/ISoftDeletable.cs
+++ src/Structr.Domain/ISoftDeletable.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Structr.Domain
 {
-    public interface ISoftDeletable
+    public interface ISoftDeletable : IAuditable
     {
         DateTime? DateDeleted { get; }
     }
