a43a36606c56479900d6400f8e1dbdb3af6ae5f5
Alexey Drapash <godhot@yandex.ru>
2020-03-19 18:17:49 +0300

    Fix AspNetCore

--- src/Structr.AspNetCore/Mvc/ControllerExtensions.cs
+++ src/Structr.AspNetCore/Mvc/ControllerExtensions.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.DependencyInjection;
-using Structr.AspNetCore.Internal;
 using Structr.AspNetCore.JavaScript;
 using System;
 using System.Collections.Generic;
