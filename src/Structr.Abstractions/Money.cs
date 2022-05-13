4f5d0c7c1d67c437623cabb144111e458528d50a
Anton Okropishin <okropishin.ae@yandex.ru>
2022-05-14 00:08:15 +0300

    Money summaries and tests added.

--- src/Structr.Abstractions/Money.cs
+++ src/Structr.Abstractions/Money.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 
 namespace Structr.Abstractions
 {
+    /// <summary>
+    /// Could be used as base class for concreete application money class.
+    /// Provides all necessary tools for working with money-like values.
+    /// </summary>
+    /// <typeparam name="TValue">Base simple type to be used for counting money.</typeparam>
     public class Money<TValue> : IEquatable<Money<TValue>>, IComparable, IComparable<Money<TValue>>
         where TValue : struct, IComparable, IComparable<TValue>
     {
@@ -11,6 +16,10 @@ namespace Structr.Abstractions
 
         protected Money() { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Money{T}"> class that.
+        /// </summary>
+        /// <param name="value">Count of currency units.</param>
         public Money(TValue value) : this()
         {
             Value = value;
@@ -21,18 +30,31 @@ namespace Structr.Abstractions
         public bool Equals(Money<TValue> other)
         {
             if (ReferenceEquals(null, other))
+            {
                 return false;
+            }
             if (ReferenceEquals(this, other))
+            {
                 return true;
+            }
 
             return EqualityComparer<TValue>.Default.Equals(this.Value, other.Value);
         }
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != GetType()) return false;
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
             return Equals((Money<TValue>)obj);
         }
 
@@ -41,7 +63,9 @@ namespace Structr.Abstractions
             unchecked
             {
                 if (Value.Equals(default(TValue)))
+                {
                     return base.GetHashCode();
+                }
 
                 return GetType().GetHashCode() ^ Value.GetHashCode();
             }
@@ -51,24 +75,45 @@ namespace Structr.Abstractions
 
         #region IComparable
 
+        /// <summary>
+        /// Performs comparision based on comparision method defined in class <see cref="TValue"/>.
+        /// </summary>
+        /// <param name="other">Object to compare with.</param>
+        /// <returns>Returns 1 if <paramref name="other"/> equals null, otherwise returns comparision result specific to <see cref="TValue"/> type.</returns>
         public int CompareTo(Money<TValue> other)
         {
             if (ReferenceEquals(null, other))
+            {
                 return 1;
+            }
 
             return Value.CompareTo(other.Value);
         }
 
+        /// <summary>
+        /// Performs comparision based on comparision method defined in class <see cref="TValue"/>.
+        /// </summary>
+        /// <param name="obj">Object to compare with.</param>
+        /// <returns>Returns 1 if <paramref name="obj"/> equals null, returns 0 if current object
+        /// ReferenceEquals <paramref name="obj"/>, otherwise returns comparision result specific
+        /// to <see cref="TValue"/> type.</returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public virtual int CompareTo(object obj)
         {
             if (ReferenceEquals(null, obj))
+            {
                 return 1;
+            }
 
             if (ReferenceEquals(this, obj))
+            {
                 return 0;
+            }
 
             if (obj.GetType() != GetType())
+            {
                 throw new InvalidOperationException(string.Format("Cannot convert object of type '{0}' to '{1}'.", obj.GetType().FullName, GetType().FullName));
+            }
 
             return CompareTo((Money<TValue>)obj);
         }
@@ -80,24 +125,32 @@ namespace Structr.Abstractions
         public static bool operator ==(Money<TValue> money1, Money<TValue> money2)
         {
             if (ReferenceEquals(null, money1))
+            {
                 return false;
+            }
             if (ReferenceEquals(null, money2))
+            {
                 return false;
+            }
 
             return money1.Equals(money2);
         }
 
         public static bool operator !=(Money<TValue> money1, Money<TValue> money2)
         {
-            return !(money1 == money2);
+            return (money1 == money2) == false;
         }
 
         public static bool operator >(Money<TValue> money1, Money<TValue> money2)
         {
             if (ReferenceEquals(null, money1))
+            {
                 return false;
+            }
             if (ReferenceEquals(null, money2))
+            {
                 return true;
+            }
 
             return money1.CompareTo(money2) > 0;
         }
@@ -109,12 +162,12 @@ namespace Structr.Abstractions
 
         public static bool operator <(Money<TValue> money1, Money<TValue> money2)
         {
-            return !(money1 >= money2);
+            return (money1 >= money2) == false;
         }
 
         public static bool operator <=(Money<TValue> money1, Money<TValue> money2)
         {
-            return !(money1 > money2);
+            return (money1 > money2) == false;
         }
 
         #endregion
@@ -125,6 +178,13 @@ namespace Structr.Abstractions
         }
     }
 
+    /// <summary>
+    /// Could be used as base class for concreete application money class.
+    /// Provides all necessary tools for working with money-like values.
+    /// Allows to take into account currencies when performing money operations.
+    /// </summary>
+    /// <typeparam name="TValue">Base simple type to be used for counting money.</typeparam>
+    /// <typeparam name="TCurrency">Type (usually enumeration) describing currency of stored money values.</typeparam>
     public class Money<TValue, TCurrency> : Money<TValue>, IEquatable<Money<TValue, TCurrency>>, IComparable, IComparable<Money<TValue, TCurrency>>
         where TValue : struct, IComparable, IComparable<TValue>
         where TCurrency : struct
@@ -133,6 +193,11 @@ namespace Structr.Abstractions
 
         protected Money() : base() { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Money{TValue, TCurrency}"> class that.
+        /// </summary>
+        /// <param name="value">Count of currency units.</param>
+        /// <param name="currency">Currency of stored money value.</param>
         public Money(TValue value, TCurrency currency) : base(value)
         {
             Currency = currency;
@@ -143,9 +208,13 @@ namespace Structr.Abstractions
         public bool Equals(Money<TValue, TCurrency> other)
         {
             if (ReferenceEquals(null, other))
+            {
                 return false;
+            }
             if (ReferenceEquals(this, other))
+            {
                 return true;
+            }
 
             return EqualityComparer<TValue>.Default.Equals(Value, other.Value) &&
                 string.Equals(Currency, other.Currency);
@@ -153,9 +222,18 @@ namespace Structr.Abstractions
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != GetType()) return false;
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
             return Equals((Money<TValue, TCurrency>)obj);
         }
 
@@ -171,13 +249,22 @@ namespace Structr.Abstractions
 
         #region IComparable
 
+        /// <summary>
+        /// Performs comparision based on comparision method defined in class <see cref="TValue"/>.
+        /// </summary>
+        /// <param name="other">Object to compare with.</param>
+        /// <returns>Returns 1 if <paramref name="other"/> equals null, otherwise returns comparision result specific to <see cref="TValue"/> type.</returns>
         public int CompareTo(Money<TValue, TCurrency> other)
         {
             if (ReferenceEquals(null, other))
+            {
                 return 1;
+            }
 
-            if (!Currency.Equals(other.Currency))
+            if (Currency.Equals(other.Currency) == false)
+            {
                 throw new InvalidOperationException(string.Format("Cannot compare {0} and {1}.", Currency, other.Currency));
+            }
 
             return Value.CompareTo(other.Value);
         }
@@ -185,13 +272,19 @@ namespace Structr.Abstractions
         public override int CompareTo(object obj)
         {
             if (ReferenceEquals(null, obj))
+            {
                 return 1;
+            }
 
             if (ReferenceEquals(this, obj))
+            {
                 return 0;
+            }
 
             if (obj.GetType() != GetType())
-                throw new InvalidOperationException(string.Format("Cannot convert object of type '{0}' to '{1}'.", obj.GetType().FullName, GetType().FullName));
+            {
+                throw new InvalidOperationException(string.Format("Can't convert object of type '{0}' to '{1}'.", obj.GetType().FullName, GetType().FullName));
+            }
 
             return CompareTo(obj as Money<TValue, TCurrency>);
         }
@@ -203,24 +296,32 @@ namespace Structr.Abstractions
         public static bool operator ==(Money<TValue, TCurrency> money1, Money<TValue, TCurrency> money2)
         {
             if (ReferenceEquals(null, money1))
+            {
                 return false;
+            }
             if (ReferenceEquals(null, money2))
+            {
                 return false;
+            }
 
             return money1.Equals(money2);
         }
 
         public static bool operator !=(Money<TValue, TCurrency> money1, Money<TValue, TCurrency> money2)
         {
-            return !(money1 == money2);
+            return (money1 == money2) == false;
         }
 
         public static bool operator >(Money<TValue, TCurrency> money1, Money<TValue, TCurrency> money2)
         {
             if (ReferenceEquals(null, money1))
+            {
                 return false;
+            }
             if (ReferenceEquals(null, money2))
+            {
                 return true;
+            }
 
             return money1.CompareTo(money2) > 0;
         }
@@ -232,12 +333,12 @@ namespace Structr.Abstractions
 
         public static bool operator <(Money<TValue, TCurrency> money1, Money<TValue, TCurrency> money2)
         {
-            return !(money1 >= money2);
+            return (money1 >= money2) == false;
         }
 
         public static bool operator <=(Money<TValue, TCurrency> money1, Money<TValue, TCurrency> money2)
         {
-            return !(money1 > money2);
+            return (money1 > money2) == false;
         }
 
         #endregion
@@ -246,7 +347,9 @@ namespace Structr.Abstractions
         {
             string currency = Currency.ToString();
             if (Currency is Enum enumCurrency)
+            {
                 currency = enumCurrency.GetDescription();
+            }
             return string.Format("{0:N2}", Value) + " " + currency;
         }
     }
---
+++ tests/Structr.Tests.Abstractions/MoneyWithCurrencyTests.cs
@@ -0,0 +1,219 @@
+using Structr.Abstractions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using FluentAssertions;
+using System.Globalization;
+
+namespace Structr.Tests.Abstractions
+{
+    public class MoneyWithCurrencyTests
+    {
+        public enum Currency
+        {
+            ABC,
+            DEF
+        }
+
+        private class MyMoney : Money<long, Currency>
+        {
+            public MyMoney(long value, Currency currency) : base(value, currency) { }
+        }
+
+        [Fact]
+        public void Creation_successful()
+        {
+            // Act
+            var result = new MyMoney(10, Currency.ABC);
+
+            // Assert
+            result.Value.Should().Be(10);
+        }
+
+        [Theory]
+        [InlineData(10, Currency.ABC, true)]
+        [InlineData(10, Currency.DEF, false)]
+        [InlineData(15, Currency.ABC, false)]
+        public void Two_instances_are_equal_if_values_and_currencies_are_equal(long value2, Currency currency2, bool expected)
+        {
+            // Act
+            var result = new MyMoney(10, Currency.ABC).Equals(new MyMoney(value2, currency2));
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Never_equals_null()
+        {
+            // Act
+            var result = new MyMoney(10, Currency.ABC).Equals(null);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Equals_itself()
+        {
+            // Arrange
+            var money = new MyMoney(10, Currency.ABC);
+
+            // Act
+            var result = money.Equals(money);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Equals_to_instance_of_same_type()
+        {
+            // Arrange
+            MyMoney money1 = new MyMoney(10, Currency.ABC);
+            object money2 = new MyMoney(10, Currency.ABC);
+
+            // Act
+            var result = money1.Equals(money2);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Not_equals_to_instance_of_another_type()
+        {
+            // Arrange
+            var money1 = new MyMoney(10, Currency.ABC);
+            var money2 = new Money<double, Currency>(10, Currency.ABC);
+
+            // Act
+            var result = money1.Equals(money2);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Comparision_to_other_defined_by_value_comparision()
+        {
+            // Act
+            var result = new MyMoney(10, Currency.ABC).CompareTo(new MyMoney(15, Currency.ABC));
+
+            // Assert
+            result.Should().Be(-1);
+        }
+
+        [Fact]
+        public void Cant_compare_with_different_currencies()
+        {
+            // Act
+            Action act = () => new MyMoney(10, Currency.ABC).CompareTo(new MyMoney(15, Currency.DEF));
+
+            // Assert
+            act.Should().Throw<InvalidOperationException>("Can't compare ABC and DEF.");
+        }
+
+        [Fact]
+        public void Cant_compare_to_instance_of_another_type()
+        {
+            // Act
+            Action act = () => new MyMoney(10, Currency.ABC).CompareTo(new Money<double, Currency>(15, Currency.ABC));
+
+            // Assert
+            act.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Comparision_to_null_gives_1()
+        {
+            // Act
+            var result = new MyMoney(10, Currency.ABC).CompareTo(null);
+
+            // Assert
+            result.Should().Be(1);
+        }
+
+        [Fact]
+        public void Comparision_to_itself_gives_0()
+        {
+            // Arrange
+            var m = new MyMoney(10, Currency.ABC);
+
+            // Act
+            var result = m.CompareTo(m);
+
+            // Assert
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Equal_if_have_equal_values_and_currencies()
+        {
+            // Arrange
+            var money1 = new MyMoney(10, Currency.ABC);
+            var money2 = new MyMoney(10, Currency.ABC);
+
+            // Act
+            var result = money1 == money2;
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(15, Currency.ABC)]
+        [InlineData(10, Currency.DEF)]
+        public void Not_equal_if_have_not_equal_values_or_currencies(long value2, Currency currency2)
+        {
+            // Arrange
+            var money1 = new MyMoney(10, Currency.ABC);
+            var money2 = new MyMoney(value2, currency2);
+
+            // Act
+            var result = money1 != money2;
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Greater_if_has_greater_value()
+        {
+            // Arrange
+            var money1 = new MyMoney(15, Currency.ABC);
+            var money2 = new MyMoney(10, Currency.ABC);
+
+            // Act
+            var result = money1 > money2;
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Lesser_if_has_lesser_value()
+        {
+            // Arrange
+            var money1 = new MyMoney(10, Currency.ABC);
+            var money2 = new MyMoney(15, Currency.ABC);
+
+            // Act
+            var result = money1 < money2;
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Could_be_represented_as_string()
+        {
+            // Act
+            var result = new MyMoney(15, Currency.ABC);
+
+            // Assert
+            var separator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            result.ToString().Should().Be($"15{separator}00 ABC");
+        }        
+    }
+}
