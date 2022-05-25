64fe2c2f87e7bb0be46c75ba93f9f58fb0721b33
Kirill Monankov <kmonankov@yandex.ru>
2022-05-25 15:08:39 +0300

    Finish XML Docs

--- src/Structr.Configuration/SettingsProviderOptions.cs
+++ src/Structr.Configuration/SettingsProviderOptions.cs
@@ -1,9 +1,18 @@
 namespace Structr.Configuration
 {
+    /// <summary>
+    /// Options for a settings provider <see cref="SettingsProvider{TSettings}"/>.
+    /// </summary>
     public class SettingsProviderOptions
     {
+        /// <summary>
+        /// Determines whether settings should be cached.
+        /// </summary>
         public bool Cache { get; set; }
 
+        /// <summary>
+        /// Initializes a new <see cref="SettingsProviderOptions"/> instance.
+        /// </summary>
         public SettingsProviderOptions()
         {
             Cache = true;
