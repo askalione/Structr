ae83faf582f9f052019739ac286e5592122ac351
Anton Okropishin <okropishin.ae@yandex.ru>
2022-06-11 23:19:42 +0300

    ClaimExtensionsTests tests added.

--- tests/Structr.Tests.Security/Extensions/ClaimExtensionsTests.cs
+++ tests/Structr.Tests.Security/Extensions/ClaimExtensionsTests.cs
@@ -13,13 +13,57 @@ namespace Structr.Tests.Security.Extensions
         [Fact]
         public void GetValue()
         {
-            // TODO
+            // Arrange
+            var claim = new Claim("Type_1", "3");
+
+            // Act
+            var result = claim.GetValue<int>();
+
+            // Assert
+            result.Should().Be(3);
         }
 
         [Fact]
-        public void TryGetValue()
+        public void GetValue_throws_when_cast_is_invalid()
         {
-            // TODO
+            // Arrange
+            var claim = new Claim("Type_1", "abc");
+
+            // Act
+            Action act = () => claim.GetValue<int>();
+
+            // Assert
+            act.Should().ThrowExactly<InvalidCastException>();
+        }
+
+        [Fact]
+        public void GetValue_throws_when_claim_is_null()
+        {
+            // Arrange
+            Claim claim = null!;
+
+            // Act
+            Action act = () => claim.GetValue<int>();
+
+            // Assert
+            act.Should().ThrowExactly<ArgumentNullException>().WithMessage("*claim*");
+        }
+
+        [Theory]
+        [InlineData("", 0, false)]
+        [InlineData("abc", 0, false)]
+        [InlineData("3", 3, true)]
+        public void TryGetValue(string val, int expectedVal, bool expected)
+        {
+            // Arrange
+            var claim = new Claim("Type_1", val);
+
+            // Act
+            var result = claim.TryGetValue(out int parsedVal);
+
+            // Assert
+            result.Should().Be(expected);
+            parsedVal.Should().Be(expectedVal);
         }
     }
 }
